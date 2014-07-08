1cb87cd6751bedc532d41bd46d93676deb18cbfb
Dave Elkan <delkan@atlassian.com>
2014-07-08 14:09:24 -0700

    DEVREL-1106 - Allow anonymous access to REST API Browser via system property.

--- src/main/java/com/atlassian/labs/restbrowser/servlet/RequiresLoginServlet.java
+++ src/main/java/com/atlassian/labs/restbrowser/servlet/RequiresLoginServlet.java
@@ -1,17 +1,18 @@
 package com.atlassian.labs.restbrowser.servlet;
 
 
-import com.atlassian.sal.api.auth.LoginUriProvider;
-import com.atlassian.sal.api.user.UserManager;
-import com.atlassian.templaterenderer.TemplateRenderer;
+import java.io.IOException;
+import java.net.URI;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.net.URI;
-import java.util.Map;
+
+import com.atlassian.sal.api.auth.LoginUriProvider;
+import com.atlassian.sal.api.user.UserManager;
+import com.atlassian.templaterenderer.TemplateRenderer;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
@@ -20,6 +21,7 @@ public abstract class RequiresLoginServlet extends HttpServlet {
     private final UserManager userManager;
     private final TemplateRenderer renderer;
     private final LoginUriProvider loginUriProvider;
+    private final static String RAB_ANONYMOUS = "rest.api.browser.anonymous";
 
     public RequiresLoginServlet(UserManager userManager, TemplateRenderer renderer, LoginUriProvider loginUriProvider) {
         this.userManager = checkNotNull(userManager, "userManager");
@@ -33,10 +35,12 @@ public abstract class RequiresLoginServlet extends HttpServlet {
 
     @Override
     protected final void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String username = userManager.getRemoteUsername(req);
-        if (username == null) {
-            redirectToLogin(req, resp);
-            return;
+        if (!Boolean.getBoolean(RAB_ANONYMOUS)) {
+            String username = userManager.getRemoteUsername(req);
+            if (username == null) {
+                redirectToLogin(req, resp);
+                return;
+            }
         }
 
         resp.setContentType("text/html;charset=utf-8");
