902e0ebe6d2e9e21c2c964ce19a884c96d98589f
Filip Rogaczewski <frogaczewski@atlassian.com>
2015-08-27 10:52:11 +0200

    JDEV-33904: Removed SOAP from JIRA

--- src/main/java/com/atlassian/labs/restbrowser/provider/JiraSoapServiceProvider.java
+++ src/main/java/com/atlassian/labs/restbrowser/provider/JiraSoapServiceProvider.java
@@ -1,58 +1,18 @@
 package com.atlassian.labs.restbrowser.provider;
 
-import com.atlassian.labs.restbrowser.plugin.AbstractSoapService;
 import com.atlassian.labs.restbrowser.plugin.SoapService;
-import com.atlassian.jira.plugin.rpc.SoapModuleDescriptor;
-import com.atlassian.plugin.PluginAccessor;
-import com.google.common.base.Function;
-import com.google.common.collect.Collections2;
 
-import java.util.Collection;
+import java.util.ArrayList;
 
 /**
  * Provides {@link SoapService}s for JIRA.
  */
 public class JiraSoapServiceProvider implements SoapServiceProvider {
 
-    private final PluginAccessor pluginAccessor;
-    
-    public JiraSoapServiceProvider(PluginAccessor pluginAccessor) {
-        this.pluginAccessor = pluginAccessor;
-    }
 
     @Override
-    public Iterable<? extends SoapService> getSoapServices() {
-        Collection<SoapModuleDescriptor> soapModuleDescriptors =
-            pluginAccessor.getEnabledModuleDescriptorsByClass(SoapModuleDescriptor.class);
-
-        return Collections2.transform(soapModuleDescriptors, transformFunction);
+    public Iterable<? extends SoapService> getSoapServices()
+    {
+        return new ArrayList<SoapService>();
     }
-
-    private final Function<SoapModuleDescriptor, JiraSoapService> transformFunction =
-            new Function<SoapModuleDescriptor, JiraSoapService>() {
-        @Override
-        public JiraSoapService apply(SoapModuleDescriptor moduleDescriptor) {
-            if (moduleDescriptor == null) return null;
-            return new JiraSoapService(moduleDescriptor);
-        }
-    };
-
-    private final class JiraSoapService extends AbstractSoapService<SoapModuleDescriptor> {
-
-        public JiraSoapService(SoapModuleDescriptor moduleDescriptor) {
-            this.moduleDescriptor = moduleDescriptor;
-        }
-
-        @Override
-        public String getServicePath() {
-            return moduleDescriptor.getServicePath();
-        }
-
-        @Override
-        public Class<?> getPublishedInterface() {
-            return moduleDescriptor.getPublishedInterface();
-        }
-
-    }
-
 }
