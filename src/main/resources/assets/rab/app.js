rdk/rest-api-browser

commit d78c6f56aff11083c1994400685819b1cab4278b

Make header bar always visible (#3)
When there was a product banner, the header bar (with the search box)
would disappear because the sidebar and the content would cover it.

The downside of this change is that the scrolling is over the whole page
instead of the sidebar having its own scroll bar.

src/main/resources/assets/rab/app.css
@@ -42,11 +42,6 @@ body {
     width:20%;
     padding:0;
     margin:0;
-    position:fixed;
-    top:82px;
-    bottom:0;
-    left:0;
-    overflow-y: scroll;
     background:#f5f5f5;
 }
 .rab-sidebar-list {
@@ -72,11 +67,6 @@ body {
 }
 .rab-main {
     width:80%;
-    position:fixed;
-    top:82px;
-    bottom:0;
-    left:0;
-    overflow-y: scroll;
     background:#fff;
     margin-left:20%;
     padding:10px 20px 20px 20px;

commit 954871e62840638ac51abe48c97474955f93baaf

RAB-21 - Fix independent scrolling for left sidebar and main content
This was regression in 3.2.0 after 3.1.3 introduced with commit d78c6f56aff11083c1994400685819b1cab4278b.

src/main/resources/assets/rab/app.css
@@ -25,13 +25,73 @@ body {
     text-align: center;
     padding:10px;
 }
-.rab-content {
-    border-bottom: 0;
+
+/* height limit maximized to window */
+body {
+    position: absolute;
+    overflow: auto;
+    height: 100%;
+    width: 100%;
 }
+body,
+#page,
+#full-height-container, #main, /*confluence*/
+#content, /*jira*/
+#content > div.aui-page-panel,
+#content > div.aui-page-panel > div.aui-page-panel-inner,
+#content > div.aui-page-panel > div.aui-page-panel-inner > section.aui-page-panel-content, /* bamboo*/
+#rab,
+.rab-content,
+.rab-sidebar {
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-flex-direction: column;
+    -ms-flex-direction: column;
+    flex-direction: column;
+}
+#page,
+#full-height-container, #main, /*confluence*/
+#content, /*jira*/
+#content > div.aui-page-panel,
+#content > div.aui-page-panel > div.aui-page-panel-inner,
+#content > div.aui-page-panel > div.aui-page-panel-inner > section.aui-page-panel-content, /* bamboo*/
+#rab,
+.rab-main,
+.rab-content,
+.rab-content > div {
+    -webkit-flex: 1 0;
+    -ms-flex: 1 0;
+    flex: 1 0;
+}
+.rab-content > div {
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+}
+.rab-sidebar,
+.rab-main {
+    position: relative;
+}
+.rab-sidebar-list,
+.rab-main > section {
+    overflow-x: auto;
+    overflow-y: scroll;
+    min-height: 100%;
+    max-height: 100%;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+}
+.rab-sidebar > nav > div {
+    width: inherit;
+}
+
 .rab-header {
     padding:5px 10px;
     background-color:#f5f5f5;
-    /*height:26px;*/
 }
 .rab-header form.aui input.text.rab-search {
     width:240px;
@@ -43,32 +103,29 @@ body {
     padding:0;
     margin:0;
     background:#f5f5f5;
-}
-.rab-sidebar-list {
-    min-height: 100%;
-    padding-bottom: 3.5em;
+    position: relative;
 }
 .rab-sidebar-item {
     text-overflow:ellipsis;
     overflow-x:hidden;
     white-space: nowrap;
 }
+.rab-divider-container {
+    position: relative;
+}
 .rab-divider {
-    position:fixed;
-    top:78px;
+    position: absolute;
     bottom:0;
     left:0;
-    margin-left:20%;
     width:8px;
     height:100%;
     z-index: 1000;
     cursor: col-resize;
     background: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAIAAAAMCAYAAABIvGxUAAADHmlDQ1BJQ0MgUHJvZmlsZQAAeAGFVN9r01AU/tplnbDhizpnEQk+aJFuZFN0Q5y2a1e6zVrqNrchSJumbVyaxiTtfrAH2YtvOsV38Qc++QcM2YNve5INxhRh+KyIIkz2IrOemzRNJ1MDufe73/nuOSfn5F6g+XFa0xQvDxRVU0/FwvzE5BTf8gFeHEMr/GhNi4YWSiZHQA/Tsnnvs/MOHsZsdO5v36v+Y9WalQwR8BwgvpQ1xCLhWaBpXNR0E+DWie+dMTXCzUxzWKcECR9nOG9jgeGMjSOWZjQ1QJoJwgfFQjpLuEA4mGng8w3YzoEU5CcmqZIuizyrRVIv5WRFsgz28B9zg/JfsKiU6Zut5xCNbZoZTtF8it4fOX1wjOYA1cE/Xxi9QbidcFg246M1fkLNJK4RJr3n7nRpmO1lmpdZKRIlHCS8YlSuM2xp5gsDiZrm0+30UJKwnzS/NDNZ8+PtUJUE6zHF9fZLRvS6vdfbkZMH4zU+pynWf0D+vff1corleZLw67QejdX0W5I6Vtvb5M2mI8PEd1E/A0hCgo4cZCjgkUIMYZpjxKr4TBYZIkqk0ml0VHmyONY7KJOW7RxHeMlfDrheFvVbsrj24Pue3SXXjrwVhcW3o9hR7bWB6bqyE5obf3VhpaNu4Te55ZsbbasLCFH+iuWxSF5lyk+CUdd1NuaQU5f8dQvPMpTuJXYSWAy6rPBe+CpsCk+FF8KXv9TIzt6tEcuAcSw+q55TzcbsJdJM0utkuL+K9ULGGPmQMUNanb4kTZyKOfLaUAsnBneC6+biXC/XB567zF3h+rkIrS5yI47CF/VFfCHwvjO+Pl+3b4hhp9u+02TrozFa67vTkbqisXqUj9sn9j2OqhMZsrG+sX5WCCu0omNqSrN0TwADJW1Ol/MFk+8RhAt8iK4tiY+rYleQTysKb5kMXpcMSa9I2S6wO4/tA7ZT1l3maV9zOfMqcOkb/cPrLjdVBl4ZwNFzLhegM3XkCbB8XizrFdsfPJ63gJE722OtPW1huos+VqvbdC5bHgG7D6vVn8+q1d3n5H8LeKP8BqkjCtbCoV8yAAAACXBIWXMAAAsSAAALEgHS3X78AAABZ2lUWHRYTUw6Y29tLmFkb2JlLnhtcAAAAAAAPHg6eG1wbWV0YSB4bWxuczp4PSJhZG9iZTpuczptZXRhLyIgeDp4bXB0az0iWE1QIENvcmUgNC40LjAiPgogICA8cmRmOlJERiB4bWxuczpyZGY9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkvMDIvMjItcmRmLXN5bnRheC1ucyMiPgogICAgICA8cmRmOkRlc2NyaXB0aW9uIHJkZjphYm91dD0iIgogICAgICAgICAgICB4bWxuczp4bXA9Imh0dHA6Ly9ucy5hZG9iZS5jb20veGFwLzEuMC8iPgogICAgICAgICA8eG1wOkNyZWF0b3JUb29sPkFkb2JlIEZpcmV3b3JrcyBDUzY8L3htcDpDcmVhdG9yVG9vbD4KICAgICAgPC9yZGY6RGVzY3JpcHRpb24+CiAgIDwvcmRmOlJERj4KPC94OnhtcG1ldGE+CsO3s+UAAAAsSURBVAgdY7x69ep/BgaGRpZNmzYBaQYGxv//QQIMDExgEpnBcu3aNQoUAwChKRhfrNwRnQAAAABJRU5ErkJggg==") no-repeat right center;
 }
-.rab-main {
-    width:80%;
+.rab-main > section {
+    display: block;
     background:#fff;
-    margin-left:20%;
     padding:10px 20px 20px 20px;
 }
 .rab-scope {
@@ -124,7 +181,6 @@ table.aui .rab-param-actions {
     border:1px solid #ccc;
     border-radius: 5px;
     background: #f5f5f5;
-    height:200px;
     font-family: Menlo, sans-serif;
     font-size:12px;
     line-height: 150%;

src/main/resources/assets/rab/app.js
@@ -47,29 +47,33 @@ angular.module('RAB', ['ngRoute', 'ngSanitize', 'ui.codemirror','ui.unique'])
     .run(function($rootScope){
         // Draggable divider
         function initDivider(){
-            var sbWidth = jQuery('.rab-sidebar').width();
-            var mainWidth = jQuery('.rab-main').width() + 40;
-            var mainMarginLeft = parseInt(jQuery('.rab-main').css('margin-left'));
-            function sizeSearchInput(sbWidth, offset){
+            function sizeSearchInput(offset) {
                 offset = offset || 0;
                 jQuery('.rab-search').css({
-                    width: sbWidth + offset - 15,
-                    maxWidth: sbWidth + offset - 15
+                    width: offset - 10,
+                    maxWidth: offset - 10
                 });
             }
-            sizeSearchInput(sbWidth);
+            function sizeMainSection(offset) {
+                var $main = jQuery('.rab-main');
+                $main.width($main.parent().width() - offset);
+            }
+            function sizeSidebar(offset) {
+                jQuery('.rab-sidebar').width(offset);
+            }
+            function sizeSections(offset) {
+                sizeSearchInput(offset);
+                sizeMainSection(offset);
+                sizeSidebar(offset);
+            }
+            sizeSections(jQuery('.rab-divider').offset().left - 1);
             jQuery('.rab-divider')
-                .css('margin-left',sbWidth + 'px')
                 .draggable({
+                    helper: function() {return jQuery('<div/>')},
                     axis: 'x',
                     cursor: 'col-resize',
                     drag: function(evt, ui){
-                        jQuery('.rab-sidebar').width(sbWidth + ui.offset.left);
-                        jQuery('.rab-main').width(mainWidth - ui.offset.left)
-                            .css('margin-left',function(){
-                                return (mainMarginLeft + ui.offset.left) + "px";
-                            });
-                        sizeSearchInput(sbWidth, ui.offset.left);
+                        sizeSections(ui.offset.left - 1);
                     }
                 });
         }

src/main/resources/templates/browser.vm
@@ -69,8 +69,10 @@ RAB.rest.services = RAB.rest.services.concat(RAB.rest.jsonRpcs);
                         </div>
                     </nav>
                 </div>
-                <div class="rab-divider"></div>
-                <section class="aui-page-panel-content rab-main" ng-view></section>
+                <div class="rab-divider-container"><div class="rab-divider"></div></div>
+                <div class="rab-main">
+                    <section class="aui-page-panel-content" ng-view></section>
+                </div>
             </div>
         </div>
     </div>